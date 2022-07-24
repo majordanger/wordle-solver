f2378547b62b63dc94da23f8db242f6fd337af85
majordanger <dbrandon.uw@gmail.com>
2022-07-23 22:24:05 -0700

    key input done.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const wordleDefaultSolutions = "TEST1\nTEST2\nTEST3\nTEST4";
 const wordleDefaultGuesses = "TEST5\nTEST6\nTEST7\nTEST8";
 const dictionarySolutions = document.querySelector("#solutions");
 const dictionaryGuesses = document.querySelector("#guesses");
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 function loadCurrentDictionaries() {
     dictionarySolutions.textContent = wordleDefaultSolutions;
@@ -46,8 +47,8 @@ for (let i = 0; i < allTiles.length; i++) {
     console.log(allTiles[i].onclick);
 }
 
-const divs = document.querySelectorAll(".wrap-tile p");
-for (let i = 0; i < divs.length; i++) {
+// const divs = document.querySelectorAll(".wrap-tile p");
+// for (let i = 0; i < divs.length; i++) {
 
     // console.log(divs[i].textContent);
     // console.log(divs[i].innerHTML);
@@ -58,11 +59,83 @@ for (let i = 0; i < divs.length; i++) {
     // console.log(divs[i].innerHTML);
     // console.log(divs[i].innerText);
     // console.log(p);
+// }
+// console.log(divs);
+
+// function formatPForTile(p) {
+//     const rawText = p.textContent;
+//     if (rawText.length === 0) return p;
+
+// }
+
+document.addEventListener("keydown", e => {
+    handleKeyInput(e);
+})
+
+function handleKeyInput(e) {
+    const focusedElem = document.activeElement;
+    if (!focusedElem) return;
+    if (focusedElem.classList.contains('tile')) {
+        const para = focusedElem.firstElementChild;
+        const textLength = para.textContent.length;
+        const key = e.key;
+        let maxLength = 0
+        if (focusedElem.classList.contains('c-tile')) {
+            maxLength = 1;
+        } else if (focusedElem.classList.contains('m-tile')) {
+            maxLength = 12;
+        } else {
+            maxLength = 26;
+        }
+        // alert(key);
+        if (key === 'Backspace' && textLength > 0) {
+            para.textContent = para.textContent.slice(0, textLength - 1);
+            formatTileText(para);
+        } else if (!(para.textContent.includes(key.toUpperCase())) && alphabet.includes(key) && textLength < maxLength) {
+            para.textContent += key;
+            formatTileText(para);
+        }
+    }
 }
-console.log(divs);
 
-function formatPForTile (p) {
+function formatTileText(p) {
     const rawText = p.textContent;
-    if (rawText.length === 0) return p;
+    const largeLetter = '40px';
+    const mediumLetter = '22px';
+    const smallLetter = '16px';
+    if (rawText.length <= 1) {
+        p.style.fontSize = largeLetter;
+        p.textContent = rawText.toUpperCase();
+    } else if (rawText.length === 2) {
+        p.style.fontSize = mediumLetter;
+        p.textContent = rawText.toUpperCase();
+    } else if (rawText.length <= 4) {
+        p.style.fontSize = mediumLetter;
+        p.innerText = rawText.splice(2, '\n').toUpperCase();
+    } else if (rawText.length <= 6) {
+        p.style.fontSize = mediumLetter;
+        p.innerText = rawText.splice(3, '\n').toUpperCase();
+    } else if (rawText.length <= 9) {
+        p.style.fontSize = smallLetter;
+        p.innerText = rawText.splice(3, '\n').splice(7, '\n').toUpperCase();
+    } else if (rawText.length <= 12) {
+        p.style.fontSize = smallLetter;
+        p.innerText = rawText.splice(4, '\n').splice(9, '\n').toUpperCase();
+    }
+}
 
+if (!String.prototype.splice) {
+    /**
+     * {JSDoc}
+     *
+     * Insert a substring at the given start index.
+     *
+     * @this {String}
+     * @param {number} start Index to splice at.
+     * @param {string} subStr String to splice in.
+     * @return {string} The new string.
+     */
+    String.prototype.splice = function (start, subStr) {
+        return this.slice(0, start) + subStr + this.slice(start);
+    };
 }
