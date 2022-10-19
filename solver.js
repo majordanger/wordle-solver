41e244bfb07d0ccab42b3b8bf939dd8c436edb3a
majordanger <dbrandon.uw@gmail.com>
2022-10-19 09:50:57 -0700

    reducespace algo coded.

--- solver.js
+++ solver.js
@@ -29,13 +29,13 @@ class Solver {
 
     #constructSolutionAndGuessSets() {
         this.solutionSet = new Set(this.solsDict);
-        this.#filterSetByKnownInfo(this.solutionSet);
+        this.#filterSetByKnownInfo(this.solutionSet, this.correctData, this.misplacedData, this.incorrectData);
 
         if (this.useKnownInfo && this.onlyValidSols) {
             this.guessSet = this.solutionSet;
         } else if (this.useKnownInfo && !this.onlyValidSols) {
             this.guessSet = new Set(this.guessDict);
-            this.#filterSetByKnownInfo(this.guessSet);
+            this.#filterSetByKnownInfo(this.guessSet, this.correctData, this.misplacedData, this.incorrectData);
             this.guessSet = new Set([...this.solutionSet, ...this.guessSet]);
         } else if (!this.useKnownInfo && !this.onlyValidSols) {
             this.guessSet = new Set([...new Set(this.solsDict), ...new Set(this.guessDict)]);
@@ -44,18 +44,18 @@ class Solver {
         }
     }
 
-    #filterSetByKnownInfo(set) {
+    #filterSetByKnownInfo(set, correct, misplaced, incorrect) {
         outer: for (const item of set) {
             for (let i = 0; i < 5; i++) {
-                if ((this.correctData[i].length === 1 && this.correctData[i] !== item[i]) || this.incorrectData.includes(item[i])) {
+                if ((correct[i].length === 1 && correct[i] !== item[i]) || incorrect.includes(item[i])) {
                     set.delete(item);
                     continue outer;
                 }
             }
             for (let i = 0; i < 5; i++) {
-                if (this.misplacedData[i].length >= 1) {
-                    for (let j = 0; j < this.misplacedData[i].length; j++) {
-                        if (this.misplacedData[i].includes(item[i]) || !item.includes(this.misplacedData[i][j])) {
+                if (misplaced[i].length >= 1) {
+                    for (let j = 0; j < misplaced[i].length; j++) {
+                        if (misplaced[i].includes(item[i]) || !item.includes(misplaced[i][j])) {
                             set.delete(item);
                             continue outer;
                         }
@@ -81,7 +81,7 @@ class Solver {
                 count++;
                 if ((Date.now() - lastTimestamp) > refreshEveryMS) {
                     lastTimestamp = Date.now();
-                    this.messageCallback(Math.floor((count/size)*100));
+                    this.messageCallback(Math.floor((count / size) * 100));
                 }
             }
 
@@ -166,59 +166,54 @@ class Solver {
                 count++;
                 if ((Date.now() - lastTimestamp) > refreshEveryMS) {
                     lastTimestamp = Date.now();
-                    this.messageCallback(Math.floor((count/size)*100));
+                    this.messageCallback(Math.floor((count / size) * 100));
                 }
             }
 
             let cumulativeScore = 0;
             for (let solWord of this.solutionSet) {
-                let wordScore = 0;
-                // We need to count the occurrences of each letter and not give score points for more repeats than the solution has.
-                // This is how Wordle provides feedback.
-                // Example: If the target word is BOSSY and the guess word is SASSY, don't give credit for the first 'S' because 
-                // it tells nothing new and Wordle will grey it out. But if the guess word was STASH. The first 'S' would be yellow.
-                const targetLetterCountMap = new Map();
+                // console.log('===============');
+                // console.log(this.solutionSet);
+                // Working variables. Start with what was passed in. We need to reset them for each new word comparison.
+                let workingSolsSet = new Set(this.solutionSet);
+                // console.log(workingSolsSet);
+                let correct = [...this.correctData];
+                let misplaced = [...this.misplacedData];
+                let incorrect = [...this.incorrectData];
 
-                // We need to keep track of which incorrect letters we've seen. The first time we learn a letter is not in a word,
-                // we have learned new information. A repeat of that letter tells us nothing.
-                const targetLetterIncorrectSet = new Set();
+                // console.log(`g: ${guessWord}`);
+                // console.log(`s: ${solWord}`);
 
-                // Check for perfect matches.
-                for (let i = 0; i < 5; i++) {
-                    // Count up letters in the solution word.
-                    if (targetLetterCountMap.has(solWord[i])) {
-                        targetLetterCountMap.set(solWord[i], targetLetterCountMap.get(solWord[i]) + 1);
-                    } else {
-                        targetLetterCountMap.set(solWord[i], 1);
-                    }
-                    // Score the matches and decrement the counters.
-                    if (solWord[i] === guessWord[i]) {
-                        wordScore += 3;
-                        // If we got a match, count the guess letter.
-                        targetLetterCountMap.set(solWord[i], targetLetterCountMap.get(solWord[i]) - 1);
+                for (let i = 0; i < guessWord.length; i++) {
+                    if (guessWord[i] === solWord[i]) {
+                        correct[i] = guessWord[i];
+                    } else if (solWord.includes(guessWord[i]) && guessWord[i] !== solWord[i] && !misplaced[i].includes(guessWord[i])) {
+                        misplaced[i] += guessWord[i];
+                    } else if (!solWord.includes(guessWord[i]) && !incorrect.includes(guessWord[i])){
+                        incorrect += guessWord[i];
                     }
                 }
 
-                // We have to score the misplaced letters AFTER the matches otherwise we could accidentally use up info on a counter
-                // for a misplaced letter when it should have been counted as a match in a later letter.
-                for (let i = 0; i < 5; i++) {
-                    // Score the misplaced letters if there is still info to be gained.
-                    if (solWord[i] !== guessWord[i] && solWord.includes(guessWord[i]) && targetLetterCountMap.get(guessWord[i]) > 0) {
-                        // Score the misplace letters.
-                        wordScore += 2;
-                        targetLetterCountMap.set(solWord[i], targetLetterCountMap.get(solWord[i]) - 1);
-                    }
-                }
+                // console.log('before:');
+                // console.log(workingSolsSet);
+                this.#filterSetByKnownInfo(workingSolsSet, correct, misplaced, incorrect);
 
-                // Score the incorrect letters ONCE each.
-                for (let i = 0; i < 5; i++) {
-                    if (!solWord.includes(guessWord[i]) && !targetLetterIncorrectSet.has(guessWord)) {
-                        wordScore += 1;
-                        targetLetterIncorrectSet.add(guessWord[i]);
-                    }
-                }
+                // solWord.includes(guessWord[i])
 
-                cumulativeScore += wordScore;
+                // console.log(`correct: ${correct}`);
+                // console.log(`misplaced: ${misplaced}`);
+                // console.log(`incorrect: ${incorrect}`);
+                
+                // console.log('after:');
+                // console.log(workingSolsSet);
+                // console.log('===============');
+                // console.log(`current set score:${workingSolsSet.size}`);
+                // console.log(`${}`);
+
+
+
+                cumulativeScore += workingSolsSet.size;
+                // console.log(`${cumulativeScore}`);
             }
 
             // Avoid NaN.
